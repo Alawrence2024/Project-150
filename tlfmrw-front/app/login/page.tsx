c9e74ab65812f88a8a9dbe4005187148d1a8fc30
Alawrence2024 <159195154+Alawrence2024@users.noreply.github.com>
2025-12-01 10:31:49 -0700

    completed the basic html for login/signup

---
+++ tlfmrw-front/app/login/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Image from "next/image";
+import "@/styles/login.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+
+
+
+
+
+export default function login() {
+
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState("");
+
+  return (
+    <main>
+      <div className="container">
+        <Image 
+          src="/loginlogo.png" 
+          alt="Logo" 
+          width={1000} 
+          height={700} 
+          style={{ margin: "-20px" }} 
+        />
+      </div>
+
+      <div className="container">
+        <input className="futuristic-input" placeholder="Username" style={{ margin: "10px"}}
+        
+          value={username}
+          onChange={(e) => {setUsername(e.target.value);}}
+        />
+        <input className="futuristic-input" placeholder="Password" type="password" 
+        
+          value={password}
+          onChange={(e) => {setPassword(e.target.value);}}
+        />
+
+        <nav style={{ paddingTop: "10px" }}>
+          <a href="/signup" style={{ color: "white" }}>
+            Create Account
+          </a>
+        </nav>
+      </div>
+    </main>
+  );
+}
+
+
---
+++ tlfmrw-front/app/signup/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import "@/styles/signup.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+
+
+
+
+
+export default function login() {
+
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState("");
+
+  return (
+    <main>
+
+    
+      <div className="container" style={{paddingTop: 100}}>
+
+        <span>
+            Free manga is waiting...
+        </span>
+
+
+        <div className="row">
+            <div className="input-wrapper">
+                <input className="futuristic-input" placeholder="Username" style={{ margin: "0px"}}
+        
+                value={username}
+                onChange={(e) => {setUsername(e.target.value);}}
+                />
+            </div>
+            <div className="input-wrapper">
+                <input className="futuristic-input" placeholder="Password" type="password" 
+        
+                value={password}
+                onChange={(e) => {setPassword(e.target.value);}}
+                />
+            </div>
+        </div>
+        <nav style={{ paddingTop: "20px" }}>
+          <a href="/login" style={{ color: "white" }}>
+            
+            Return
+          </a>
+        </nav>
+      </div>
+    </main>
+  );
+}
+
+
