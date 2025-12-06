698ef21de2909c2cf8f14b4edc196b44fdfa6133
Josh <joboshua15@gmail.com>
2025-12-06 12:08:56 -0700

    dal and api routes implemented

--- tlfmrw-front/app/api/signup/route.js
+++ tlfmrw-front/app/api/signup/route.js
@@ -1,11 +1,34 @@
-"use server"
-
 import { DAL } from "../../../dals/PersistentRedisDAL"
+import { C_DAL } from "../../../dals/CachingRedisDal"
 import { ENCRYPT } from "../../../encryption/encrypt"
 
 export async function POST(request) {
     const { username, password } = await request.json()
     const encryptedPassword = await ENCRYPT.encrypt(password)
     await DAL.CreateUser(username, encryptedPassword)
-    return new Response({success: "true"})
+
+    return new Response(
+        JSON.stringify({ success: true }),
+        { headers: { "Content-Type": "application/json" } }
+    )
+}
+
+export async function GET(request) {
+    const { searchParams } = new URL(request.url)
+    const username = searchParams.get("username")
+
+    if (!username) {
+        return new Response(
+            JSON.stringify({ success: false, error: "username is required" }),
+            { status: 400 }
+        )
+    }
+
+    const user = await DAL.GetUser(username)
+    console.log(user)
+
+    return new Response(
+        JSON.stringify({ success: !!user }),
+        { headers: { "Content-Type": "application/json" } }
+    )
 }
--- tlfmrw-front/dals/PersistentRedisDAL.js
+++ tlfmrw-front/dals/PersistentRedisDAL.js
@@ -1,15 +1,16 @@
 import Redis from "ioredis"
 
+const redis = new Redis({
+    host: "redis",
+    port: 6379
+})
+
+// #region Helper Functions
 const KEY_NAMES = {
     USER: "user",
     FAVORITES: "favorites",
     READ: "read"
 }
-
-const redis = new Redis({
-    host: "redis",
-    port: 6379
-})
 
 function formatKey(...strings) {
     return strings.join(":")
@@ -26,6 +27,7 @@ function buildUserFavoritesKey(username) {
 function buildUserReadKey(username, mangaId) {
     return formatKey(buildUserKey(username), mangaId, KEY_NAMES.READ)
 }
+// #endregion
 
 export const DAL = {
     async CreateUser(username, password) {
@@ -36,8 +38,9 @@ export const DAL = {
     },
 
     async GetUser(username) {
+        console.log("DAL.GetUser:", username)
         const userData = await redis.hgetall(buildUserKey(username))
-        return Object.keys(userData).length === 0 ? null : data
+        return Object.keys(userData).length === 0 ? null : userData
     },
 
     async SetUserFavorite(username, mangaId) {
