44af58563842139436a3dd9c1e3a1d86cbbcad1d
Josh <joboshua15@gmail.com>
2025-12-06 12:43:32 -0700

    resolve merge conflict

--- tlfmrw-front/app/library/page.tsx
+++ tlfmrw-front/app/library/page.tsx
@@ -1,53 +1,56 @@
 "use client";
 
-import Image from "next/image";
-import "@/styles/login.css";
+import "@/styles/library.css";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect,useState } from "react";
 
-
-
-
-
+interface Manga {
+  title: string;
+  cover: string;
+}
 
 export default function library() {
+const [covers, setCovers] = useState<Manga[]>([]);
 
-const [username, setUsername] = useState("");
-const [password, setPassword] = useState("");
-const [error, setError] = useState("");
+  useEffect(() => {
+    async function fetchCovers() {
+      const res = await fetch("");
+      const data: Manga[] = await res.json();
+      setCovers(data);
+    }
+    fetchCovers();
+  }, []);
 
   return (
     <main>
-      <div className="container">
-        <Image 
-          src="/loginlogo.png" 
-          alt="Logo" 
-          width={1000} 
-          height={700} 
-          style={{ margin: "-20px" }} 
-        />
-      </div>
+      <div className="master_container">
+        <div className="sidebar">
+        </div>
+        <div className="centered_container">
+          <div className="scrollview" id="mangaDisplay">
+            {covers.map((manga, i) => (
+              <a>
+                <img
+                key={i}
+                src={manga.cover}
+                width={200}
+                height={300}
+                alt={manga.title}
+                className="cover"
+              />
+              </a>
 
-      <div className="container">
-        <input className="futuristic-input" placeholder="Username" style={{ margin: "10px"}}
-        
-          value={username}
-          onChange={(e) => {setUsername(e.target.value);}}
-        />
-        <input className="futuristic-input" placeholder="Password" type="password" 
-        
-          value={password}
-          onChange={(e) => {setPassword(e.target.value);}}
-        />
 
-        <nav style={{ paddingTop: "10px" }}>
-          <a href="/signup" style={{ color: "white" }}>
-            Create Account
-          </a>
-        </nav>
+              
+
+              
+            ))}
+          </div>
+        </div>
+        <div className="sidebar">
+        </div>
       </div>
     </main>
   );
 }
-
 
